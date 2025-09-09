766a47c0bc4c124c22978318bdaf8b0e039c6d8b
yangwei <79722513@qq.com>
2025-09-10 00:48:54 +0800

    - rustore iap server purchase verify

--- payment/internal/verifier/def.go
+++ payment/internal/verifier/def.go
@@ -8,21 +8,41 @@ import (
 
 type Factory func(cp config.ChannelProperty) (inf.Verifier, error)
 
-var factories = map[string]Factory{
-	"rustore": func(cp config.ChannelProperty) (inf.Verifier, error) {
+var factories = make(map[string]Factory)
+
+func init() {
+	//
+	RegisterFactory("rustore", func(cp config.ChannelProperty) (inf.Verifier, error) {
 		return vk.NewRustore(vk.RustoreConfig{
 			ClientID:     cp.ClientID,
 			ClientSecret: cp.ClientSecret,
 			IsSandbox:    cp.Sandbox,
 			Apps:         cp.Apps,
 		})
-	},
-	"huawei": func(cp config.ChannelProperty) (inf.Verifier, error) {
-		return nil, nil
-	},
-	"xiaomi": func(cp config.ChannelProperty) (inf.Verifier, error) {
-		return nil, nil
-	},
+	})
+	//
+	RegisterFactory("huawei", func(cp config.ChannelProperty) (inf.Verifier, error) {
+		return vk.NewRustore(vk.RustoreConfig{
+			ClientID:     cp.ClientID,
+			ClientSecret: cp.ClientSecret,
+			IsSandbox:    cp.Sandbox,
+			Apps:         cp.Apps,
+		})
+	})
+	//
+	RegisterFactory("xiaomi", func(cp config.ChannelProperty) (inf.Verifier, error) {
+		return vk.NewRustore(vk.RustoreConfig{
+			ClientID:     cp.ClientID,
+			ClientSecret: cp.ClientSecret,
+			IsSandbox:    cp.Sandbox,
+			Apps:         cp.Apps,
+		})
+	})
+}
+
+// RegisterFactory register a custom verifier factory
+func RegisterFactory(name string, factory Factory) {
+	factories[name] = factory
 }
 
 func FindVerifier(c config.Channel) (inf.Verifier, error) {
