c02d35ddba784d252be4f00e03c5088d3f5ca5a4
yangwei <79722513@qq.com>
2025-09-12 22:37:03 +0800

    - dokcerfile

--- pkg/caches/local.go
+++ pkg/caches/local.go
@@ -1,9 +1,10 @@
 package caches
 
 import (
+	"sync"
+
 	"gitee.com/ywengineer/smart-kit/pkg/utilk"
 	"github.com/dgraph-io/ristretto/v2"
-	"sync"
 )
 
 var cache *ristretto.Cache[string, []byte]
@@ -23,6 +24,7 @@ func NewCache[T any](capacity int64) (*ristretto.Cache[string, T], error) {
 	c, err := ristretto.NewCache(&ristretto.Config[string, T]{
 		NumCounters: 1e7,      // number of keys to track frequency of (10M).
 		MaxCost:     capacity, // maximum cost of cache (1GB).
+		BufferItems: 64,       // size of Get buffers.
 	})
 	if err != nil {
 		return nil, err
